e4183f65b2d5350475b50997b75c45b679eba709
moconnel <matthew.oconnell@nrel.gov>
2016-04-21 10:54:14 -0600

    changed run_html_output for rstudio

--- run_html_output.R
+++ run_html_output.R
@@ -29,12 +29,12 @@ if (interactive()) {
 # USER INPUT: set input parameters ----
 #------------------------------------------------------------------------------|
 
+setwd('C:/users/moconnel/documents/PLEXOS-Vis')
+input.csv = '//plexossql/data/moconnel/flexibility/html/input_data.csv'
 # point to location of PSSE2PLEXOS master script
 master.script.dir <- 'PLEXOS-Vis'
-input.csv = 'vis_input_data.csv'
 # Sourcing the setup file and required functions
 source(file.path(master.script.dir,'query_functions.R'))
 source(file.path(master.script.dir,'setup_plexosAnalysis.R'))
 output.dir = file.path(na.exclude(inputs$Fig.Path))
-render(input=file.path(master.script.dir,'HTML_output.Rmd'), c("html_document"),
-       output_dir = output.dir)
+render(input=file.path(master.script.dir,'HTML_output.Rmd'), c("html_document"), output_dir = output.dir)
