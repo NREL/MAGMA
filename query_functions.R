c445a7f807a19bece7021b78b49b463ac7d4da9c
moconnel <matthew.oconnel@nrel.gov>
2016-06-14 15:39:44 -0600

    finished commenting code

--- query_functions.R
+++ query_functions.R
@@ -6,7 +6,9 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Query General Data
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# These functions are called from the setup data queries file. They use the rplexos package to query the solution database.
 
+# Full run generator data.
 yr_gen_query = function(database) {
   yr.data.generator = select(query_year(database, 'Generator', 
                                         prop = c('Generation', 'Available Energy', 'Emissions Cost', 'Fuel Cost', 'Start & Shutdown Cost', 'VO&M Cost', 'Installed Capacity'),
@@ -14,53 +16,63 @@ yr_gen_query = function(database) {
   return(yr.data.generator)
 }
 
+# Full run region data
 yr_region_query = function(database) {
   yr.data.region = select(query_year(database, 'Region', c('Load', 'Imports', 'Exports', 'Unserved Energy')), property, name, value)
   return(yr.data.region)
 }
   
+# Full run zone data
 yr_zone_query = function(database) {
   yr.data.zone = select(query_year(database, 'Zone', c('Load', 'Imports', 'Exports', 'Unserved Energy')), property, name, value)
   return(yr.data.zone)
 }
   
+# Full run reserves provision and shortage
 yr_reserve_query = function(database) {
   yr.data.reserve = select(query_year(database, 'Reserve', c('Provision', 'Shortage')), property, name, value)
   return(yr.data.reserve)
 }
 
+# Full run interface flows
 yr_interface_query = function(database) {
   yr.data.interface = select(query_year(database, 'Interface', 'Flow'), property, name, value, time)
   return(yr.data.interface)
 }
 
+# Interval level generator generation
 int_gen_query = function(database) {
   int.data.gen = select(query_interval(database, 'Generator', 'Generation', columns = c('category', 'name')), 
                               property, name, value, time, category)
   return(int.data.gen)
 }
 
+# Interval level generator capacity
 int_avail_cap_query = function(database) {
   int.data.avail.cap = select(query_interval(database, 'Generator', 'Available Capacity', columns = c('category', 'name')), 
                               property, name, value, time, category)
   return(int.data.avail.cap)
 }
 
+# Interval level region load and price
 int_region_query = function(database) {
   int.data.region = select(query_interval(database, 'Region', c('Load', 'Price')), property, name, time, value)
   return(int.data.region)
 }
 
+# Interval level zone load
 int_zone_query = function(database) {
   int.data.zone = select(query_interval(database, 'Zone', 'Load'), property, name, time, value)
   return(int.data.zone)
 }
 
+# Interval level interface flows
 int_interface_query = function(database) {
   int.data.interface = select(query_interval(database, 'Interface', 'Flow'), property, name, time, value)
   return(int.data.interface)
 }
 
+# Interval level reserve provisions
 int_reserve_query = function(database) {
   int.data.reserve = select(query_interval(database, 'Reserve', 'Provision'), property, name, time, value)
   return(int.data.reserve)
@@ -69,11 +81,13 @@ int_reserve_query = function(database) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Generation by type
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# This function returns total generation by type and curtailment. Curtailment is calculated according to the renewable types specified in the input file. 
 
 gen_by_type = function(yr.data.gen) {
   
   yr.data = yr.data.gen
   
+  # Filter out generation and available capacity data and add generation type by matching generator name. 
   if ( use.gen.type.mapping.csv ) {
     yr.gen = yr.data %>%
       filter(property == 'Generation') %>%
@@ -87,6 +101,7 @@ gen_by_type = function(yr.data.gen) {
       filter(Type %in% re.types ) %>%
       dcast(property ~ Type, sum)
     
+    # Same as above section except this is for if you are using the PLEXOS category to match generation type instead of a mapping CSV file.
   } else {
     yr.gen = yr.data %>%
       filter(property == 'Generation') %>%
@@ -102,19 +117,23 @@ gen_by_type = function(yr.data.gen) {
   }
   avail = avail[,2:ncol(avail)]
   
+  # Pull out generation data for types used in curtailment calculation.
   re.gen = yr.gen %>%
     filter(Type %in% re.types ) %>%
     dcast(property ~ Type, sum)
   re.gen = re.gen[,2:ncol(re.gen)]
 
+  # Sum up generation by type
   yr.gen = yr.gen %>%
     dcast(Type ~ property, sum) %>%
     rename(GWh = Generation) %>%
     ddply('Type', numcolwise(sum)) 
     
+  # Calculate curtailment
   curt = avail - re.gen
   curt.tot = sum(curt)
 
+  # Combine everything before returning the resulting data.
   yr.gen = rbind(yr.gen, data.frame(Type = 'Curtailment', GWh = curt.tot))
   
   return(yr.gen)
@@ -123,11 +142,14 @@ gen_by_type = function(yr.data.gen) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Region and Zone Generation by type according to generator name
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- 
+# This function returns total generation separated by type but also by region and zone.  
+
 region_zone_gen = function(yr.data.gen) {
   
   r.z.gen = yr.data.gen
   
+  # Filter out generation and available capacity data and add generation type by matching generator name.
+  # Also add region and zone by matching generator name in the region and zone mapping file. 
   if ( use.gen.type.mapping.csv ) {
     gen.data = r.z.gen %>%
       filter(property=='Generation') %>%
@@ -143,6 +165,7 @@ region_zone_gen = function(yr.data.gen) {
       filter(Type %in% re.types) %>%
       select(name, Avail = value)
     
+    # Same as above section except this is for if you are using the PLEXOS category to match generation type instead of a mapping CSV file.
   } else {
     gen.data = r.z.gen %>%
       filter(property=='Generation') %>%
@@ -159,6 +182,7 @@ region_zone_gen = function(yr.data.gen) {
       select(name, Avail = value)
   }
 
+  # Curtailment calculation based on renewable types specified in input file
   curt = gen.data %>%
     filter(Type %in% re.types ) %>%
     join(avail.data, by = 'name')
@@ -167,6 +191,7 @@ region_zone_gen = function(yr.data.gen) {
   curt$value = curt$Avail - curt$value
   curt$Avail = NULL
   
+  # Combine generation and curtailment and return.
   gen.data = rbind(gen.data, curt)
 
   return(gen.data)
@@ -177,9 +202,11 @@ region_zone_gen = function(yr.data.gen) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Key Period Generation by Type 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# This function returns interval level generation and curtailment used for the key period time series dispatch stacks. 
 
 interval_gen = function(int.data.region, int.data.zone, int.data.gen, int.data.avail.cap) {
   
+  # Either sum up load for each region or each zone, depending on which there are more of. 
   if (length(region.names)>=length(zone.names)){
     load = dcast(int.data.region, time+name~property, value.var = 'value', fun.aggregate = sum)
     colnames(load)[colnames(load)=='name'] = 'Region'
@@ -194,6 +221,7 @@ interval_gen = function(int.data.region, int.data.zone, int.data.gen, int.data.a
   
   colnames(load)[colnames(load)=='property'] = 'Type'
   
+  # Pull out interval generation data, and add generation type and region and zone according to generator name. Then add load data.
   if ( use.gen.type.mapping.csv ) {
     int.gen = int.data.gen %>%
       filter(property == 'Generation') %>%
@@ -217,8 +245,10 @@ interval_gen = function(int.data.region, int.data.zone, int.data.gen, int.data.a
   int.gen = merge(int.gen[,!names(int.gen) %in% names(rz.unique)[names(rz.unique) != spatialcol]],
                   rz.unique,by=spatialcol,all.y=T)
   
+  # Pull out renewable data for curtilment calculations. 
   re.gen = subset(int.gen, select = c('time',spatialcol,re.types))
 
+  # Pull out interval generation capacity and add generation type, region, and zone based on matching generator names.
   if ( use.gen.type.mapping.csv ) {
     int.avail = int.data.avail.cap %>%
       filter(property == 'Available Capacity') %>%
@@ -240,6 +270,7 @@ interval_gen = function(int.data.region, int.data.zone, int.data.gen, int.data.a
       join(re.gen[,c('time',spatialcol)],by=c('time',spatialcol),type='full')
   }
   
+  # Calculate curtailment and add it to generation and load data from above. 
   int.avail[is.na(int.avail)] = 0
   re.gen[is.na(re.gen)] = 0
   curtailed = merge(int.avail,re.gen,by=c('time',spatialcol))
@@ -257,12 +288,14 @@ interval_gen = function(int.data.region, int.data.zone, int.data.gen, int.data.a
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Total Curtailment
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Calculates interval level total curtailment
 
 daily_curtailment = function(int.data.gen, int.data.avail.cap) {
   
   gen.data = int.data.gen
   avail.data = int.data.avail.cap
   
+  # Separate generation and available capacity data by type for each interval.
   if ( use.gen.type.mapping.csv ) {
     c.gen = gen.data %>%
       filter(property == 'Generation') %>%
@@ -278,6 +311,7 @@ daily_curtailment = function(int.data.gen, int.data.avail.cap) {
       dcast(time ~ Type, sum) %>%
       subset(select = re.types)
     
+    # Below does the same thing as above except matches generation type using PLEXOS categories instead of a mapping file.
   } else {
     c.gen = gen.data %>%
       filter(property == 'Generation') %>%
@@ -294,6 +328,7 @@ daily_curtailment = function(int.data.gen, int.data.avail.cap) {
       subset(select = re.types)  
   }
 
+  # Summing up total curtailment for each interval
   curt = c.avail - c.gen
   curt.tot = data.frame(rowSums(curt))
   colnames(curt.tot) = 'Curtailment'
@@ -307,6 +342,7 @@ daily_curtailment = function(int.data.gen, int.data.avail.cap) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Cost 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Returns a table of total run costs
 
 costs = function(yr.data.generator) {
 
@@ -337,6 +373,7 @@ return(cost.table)
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Annual Reserve Provisions
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Calculates total reserve provision and shortage for each reserve type. 
 
 annual_reserves = function(yr.data.reserve) {
   
@@ -356,6 +393,7 @@ annual_reserves = function(yr.data.reserve) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Interval Reserve Provisions 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Calculates the interval level reserve provision
 
 interval_reserves = function(int.data.reserve) {
   provision = int.data.reserve
@@ -366,6 +404,7 @@ interval_reserves = function(int.data.reserve) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Interface Flows 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Total run and interval level interface flow data, for specific interfaces that are specified in the input file. 
 
 annual_interface_flows = function(yr.data.interface) {
   
@@ -393,6 +432,8 @@ interval_interface_flows = function(int.data.interface) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Region and Zone Stats
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# This function sums up region and zone stats for the entire run. 
+# zones are defined either in PLEXOS, or using the region and zone mapping file. 
 
 region_stats = function(yr.data.region) {
   r.data = yr.data.region
@@ -419,6 +460,7 @@ zone_stats = function(yr.data.region, yr.data.zone) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Region and Zone Load
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Returns region level and zone level load data for the entire run. 
 
 region_load = function(yr.data.region) {
   r.data = yr.data.region
@@ -450,11 +492,13 @@ zone_load = function(yr.data.region, yr.data.zone) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Capacity Factor
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Calculates the capacity factor of all the generation types for the full run. 
 
 capacity_factor = function(yr.data.generator) {
   
   cf = yr.data.generator
   
+  # Pull out installed capacity and generation and match them to generation type by generator name. 
   if ( use.gen.type.mapping.csv ) {
     mc = cf %>%
       filter(property == 'Installed Capacity') %>%
@@ -466,6 +510,7 @@ capacity_factor = function(yr.data.generator) {
       rename(Gen = value) %>%
       join(gen.type.mapping, by = 'name')
     
+    # Same as above, but matches generation type according to PLEXOS type. 
   } else {
     mc = cf %>%
       filter(property == 'Installed Capacity') %>%
@@ -480,12 +525,14 @@ capacity_factor = function(yr.data.generator) {
     
   mc$Type = factor(mc$Type, levels = rev(c(gen.order)))
   
+  # Calculates generation type total capacity and generation for the full run
   c.factor = mc %>%
     select(name, MaxCap, Type) %>%
     join(gen[,c('name', 'Gen')], by = 'name') %>%
     select(Type, MaxCap, Gen) %>%
     ddply('Type', summarise, MaxCap=sum(MaxCap), Gen=sum(Gen))  
 
+  # Calculate capacity factor for each generation type
   n.int = length(seq(from = first.day, to = last.day, by = 'day'))*intervals.per.day
   c.factor$`Capacity Factor (%)` = c.factor$Gen/(c.factor$MaxCap/1000*n.int)*100
   
@@ -498,6 +545,7 @@ capacity_factor = function(yr.data.generator) {
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Committed capacity 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# This function just pulls out available capacity at the interval level for use int he DA-RT committment and dispatch plots
 
 cap_committed = function(int.data.commit) {
   
@@ -507,6 +555,7 @@ cap_committed = function(int.data.commit) {
     spatialcol = "Zone"    
   }
   
+  # Query available capacity at the interval level, add generation type and region and zone by matching mapping file with generator names.
   if ( use.gen.type.mapping.csv ) {
     commit.data = int.data.commit %>%
       select(time, name, category, value) %>%
