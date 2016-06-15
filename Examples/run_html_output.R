1f1a2a2f1b161357fd9cfd0761d1737896624360
moconnel <matthew.oconnel@nrel.gov>
2016-06-15 15:06:06 -0600

    misc

--- Examples/run_html_output.R
+++ Examples/run_html_output.R
@@ -3,11 +3,11 @@
 # USER INPUT: set input parameters ----
 #------------------------------------------------------------------------------|
 
-setwd('C:/Users/moconnel/documents/MAGMA')
+setwd('<Main directory where MAGMA files are located>')
 master.script.dir <- 'MAGMA'
-input.csv = 'C:/Users/moconnel/documents/MAGMA/Examples/input_data_example.csv'
+input.csv = '<This should point to your input data CSV file>'
 # Sourcing the setup file and required functions
 source(file.path('query_functions.R'))
 source(file.path('setup_plexosAnalysis.R'))
-output.dir = file.path('Examples/reports')
+output.dir = file.path('<Directory where HTML reports should be saved>')
 render(input=file.path('HTML_output.Rmd'), c("html_document"), output_dir = output.dir)
