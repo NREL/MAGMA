054f2569bb08c91aa361cf6b6e91d8a5e962e5c4
O'Connell <moconnel@nrel.gov>
2016-06-15 11:41:28 -0600

    adding reports folder

--- run_html_output.R
+++ run_html_output.R
@@ -3,12 +3,11 @@
 # USER INPUT: set input parameters ----
 #------------------------------------------------------------------------------|
 
-setwd('C:/Users/moconnel/documents/PLEXOS-Vis')
-# point to location of PSSE2PLEXOS master script
-master.script.dir <- 'PLEXOS-Vis'
-input.csv = '//plexossql/data/moconnel/MAGMA/input_data.csv'
+setwd('C:/Users/moconnel/documents/MAGMA')
+master.script.dir <- 'MAGMA'
+input.csv = 'C:/Users/moconnel/documents/MAGMA/Examples/input_data.csv'
 # Sourcing the setup file and required functions
 source(file.path('query_functions.R'))
 source(file.path('setup_plexosAnalysis.R'))
-output.dir = file.path((na.exclude(inputs$Fig.Path)))
+output.dir = file.path('Examples/reports')
 render(input=file.path('HTML_output.Rmd'), c("html_document"), output_dir = output.dir)
