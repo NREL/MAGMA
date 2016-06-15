84a124cd7d5200d25b8c46b2976606e40f196bfa
moconnel <matthew.oconnel@nrel.gov>
2016-06-15 11:32:20 -0600

    cleanin up files

--- run_html_output.R
+++ run_html_output.R
@@ -1,31 +1,5 @@
 
 #------------------------------------------------------------------------------|
-# set working directory ----
-#------------------------------------------------------------------------------|
-
-# set working directory
-
-# if (interactive()) {
-#   t=try(dirname(sys.frame(1)$ofile),silent = T)
-#   if(inherits(t, "try-error")) {
-#     warning("Make sure you are in the PLEXOS-Vis submodule path")
-#   } else {
-#     script.dir = dirname(sys.frame(1)$ofile)
-#     setwd(script.dir)
-#   }
-# } else {
-#   dir = getSrcDirectory(function(x) {x})
-#   m <- regexpr("(?<=^--file=).+", commandArgs(), perl=TRUE)
-#   script.dir <- dirname(regmatches(commandArgs(), m))
-#   if(length(script.dir) == 0) 
-#     stop("can't determine script dir: please call the script with Rscript")
-#   if(length(script.dir) > 1) 
-#     stop("can't determine script dir: more than one '--file' argument detected")
-#   setwd(script.dir)
-# }
-
-
-#------------------------------------------------------------------------------|
 # USER INPUT: set input parameters ----
 #------------------------------------------------------------------------------|
 
