46740e041eaec50b9fd37febed221d37fa6aa322
moconnel <matthew.oconnel@nrel.gov>
2016-06-22 12:25:14 -0600

    adding filename output to run_html_output script

--- Examples/run_html_output.R
+++ Examples/run_html_output.R
@@ -4,10 +4,9 @@
 #------------------------------------------------------------------------------|
 
 setwd('<Main directory where MAGMA files are located>')
-master.script.dir <- 'MAGMA'
 input.csv = '<This should point to your input data CSV file>'
 # Sourcing the setup file and required functions
 source(file.path('query_functions.R'))
 source(file.path('setup_plexosAnalysis.R'))
 output.dir = file.path('<Directory where HTML reports should be saved>')
-render(input=file.path('HTML_output.Rmd'), c("html_document"), output_dir = output.dir)
+render(input=file.path('HTML_output.Rmd'), c("html_document"), output_file='<Desired output file name. Include .html at end.>', output_dir = output.dir)
