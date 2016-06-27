5738f4f0d867ed5d999febf16454578de448bb4a
O'Connell <moconnel@nrel.gov>
2016-06-27 12:14:08 -0600

    added setup_dataQueries file to run_html_output

--- Examples/run_html_output.R
+++ Examples/run_html_output.R
@@ -8,5 +8,6 @@ input.csv = '<This should point to your input data CSV file>'
 # Sourcing the setup file and required functions
 source(file.path('query_functions.R'))
 source(file.path('setup_plexosAnalysis.R'))
+source(file.path('setup_dataQueries.R'))
 output.dir = file.path('<Directory where HTML reports should be saved>')
 render(input=file.path('HTML_output.Rmd'), c("html_document"), output_file='<Desired output file name. Include .html at end.>', output_dir = output.dir)
