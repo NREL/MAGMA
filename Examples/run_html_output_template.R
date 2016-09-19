883736343fe8648ccfa97ca481a853671e81e5d7
bstoll <brady.stoll@nrel.gov>
2016-09-19 17:25:10 -0600

    Updated run_html_output examples to be consistent

--- Examples/run_html_output_template.R
+++ Examples/run_html_output_template.R
@@ -2,12 +2,17 @@
 #------------------------------------------------------------------------------|
 # USER INPUT: set input parameters ----
 #------------------------------------------------------------------------------|
-
-setwd('<Main directory where MAGMA files are located>')
+magma.dir = '<Main directory where MAGMA files are located>'
 input.csv = '<This should point to your input data CSV file>'
+output.dir = '<Directory where HTML reports should be saved>'
+output.name = '<Desired output file name. Include .html at end.>'
+#------------------------------------------------------------------------------|
+# Run code to create HTML
+#------------------------------------------------------------------------------|
+setwd(magma.dir)
 # Sourcing the setup file and required functions
 source(file.path('query_functions.R'))
 source(file.path('setup_plexosAnalysis.R'))
 source(file.path('setup_dataQueries.R'))
-output.dir = file.path('<Directory where HTML reports should be saved>')
-render(input=file.path('HTML_output.Rmd'), c("html_document"), output_file='<Desired output file name. Include .html at end.>', output_dir = output.dir)
+render(input=file.path('HTML_output.Rmd'), c("html_document"), 
+	   output_file=output.name, output_dir = file.path(output.dir))
