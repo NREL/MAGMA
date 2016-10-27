56e360f6dab66ffcc705847f526e68e545f5c43e
O'Connell <moconnel@nrel.gov>
2016-10-27 14:39:57 -0600

    edits to user interface

--- UserInterface_InputData/server.R
+++ UserInterface_InputData/server.R
@@ -129,55 +129,65 @@ shinyServer(function(input, output) {
     start.time = start.end.time[seq(1,length(start.end.time),2)]
     end.time = start.end.time[seq(2,length(start.end.time),2)]
 
-    write_data = list( inputDatabase.Location = input$db.loc,
-                            DayAhead.Database.Location = input$DA.RT.db.loc,
-                            Using.Gen.Type.Mapping.CSV = genTypeMapping,
-                            reassign.zones = input$reassignZones,
-                            Gen.Region.Zone.Mapping.Filename = input$regionZoneMappingFile,
-                            CSV.Gen.Type.File.Location = input$genTypeMappingLocation,
-                            PLEXOS.Gen.Category = c( input$plexosCategory1,  input$plexosCategory2,  input$plexosCategory3,
-                                                     input$plexosCategory4,  input$plexosCategory5,  input$plexosCategory6,
-                                                     input$plexosCategory7,  input$plexosCategory8,  input$plexosCategory9,
-                                                     input$plexosCategory10, input$plexosCategory11, input$plexosCategory12,
-                                                     input$plexosCategory13, input$plexosCategory14, input$plexosCategory15 ),
-                            PLEXOS.Desired.Type = c( input$genType1,  input$genType2,  input$genType3,  input$genType4,  input$genType5,
-                                                     input$genType6,  input$genType7,  input$genType8,  input$genType9,  input$genType10,
-                                                     input$genType11, input$genType12, input$genType13, input$genType14, input$genType15 ),
-                            Gen.Type = c( input$genOrder1,  input$genOrder2,  input$genOrder3,  input$genOrder4,  input$genOrder5,  input$genOrder6,  input$genOrder7,
-                                          input$genOrder8,  input$genOrder9,  input$genOrder10, input$genOrder11, input$genOrder12, input$genOrder13, input$genOrder14,
-                                          input$genOrder15, input$genOrder16, input$genOrder17, input$genOrder18, input$genOrder19, input$genOrder20 ),
-                            Plot.Color = c( input$genTypeColor1,  input$genTypeColor2,  input$genTypeColor3,  input$genTypeColor4,  input$genTypeColor5, 
-                                            input$genTypeColor6,  input$genTypeColor7,  input$genTypeColor8,  input$genTypeColor9,  input$genTypeColor10, 
-                                            input$genTypeColor11, input$genTypeColor12, input$genTypeColor13, input$genTypeColor14, input$genTypeColor15, 
-                                            input$genTypeColor16, input$genTypeColor17, input$genTypeColor18, input$genTypeColor19, input$genTypeColor20 ),
-                            Gen.Order = c( input$genOrder1,  input$genOrder2,  input$genOrder3,  input$genOrder4,  input$genOrder5,  input$genOrder6,  input$genOrder7,
-                                           input$genOrder8,  input$genOrder9,  input$genOrder10, input$genOrder11, input$genOrder12, input$genOrder13, input$genOrder14,
-                                           input$genOrder15, input$genOrder16, input$genOrder17, input$genOrder18, input$genOrder19, input$genOrder20 ),
-                            Renewable.Types.for.Curtailment = c( input$reType1,  input$reType2,  input$reType3,  input$reType4,  input$reType5, 
-                                                                 input$reType6,  input$reType7,  input$reType8,  input$reType9,  input$reType10,
-                                                                 input$reType11, input$reType12, input$reType13, input$reType14, input$reType15, 
-                                                                 input$reType16, input$reType17, input$reType18, input$reType19, input$reType20 ),	
-                            DA.RT.Plot.Types = c( input$rDA_RT_Type1,  input$rDA_RT_Type2,  input$rDA_RT_Type3,  input$rDA_RT_Type4,  input$rDA_RT_Type5, 
-                                                  input$rDA_RT_Type6,  input$rDA_RT_Type7,  input$rDA_RT_Type8,  input$rDA_RT_Type9,  input$rDA_RT_Type10, 
-                                                  input$rDA_RT_Type11, input$rDA_RT_Type12, input$rDA_RT_Type13, input$rDA_RT_Type14, input$rDA_RT_Type15, 
-                                                  input$rDA_RT_Type16, input$rDA_RT_Type17, input$rDA_RT_Type18, input$rDA_RT_Type19, input$rDA_RT_Type20 ),	
-                            Key.Periods = c( input$keyPeriodName1, input$keyPeriodName2, input$keyPeriodName3, input$keyPeriodName4, 
-                                             input$keyPeriodName5, input$keyPeriodName6, input$keyPeriodName7, input$keyPeriodName8 ),
-                            Start.Time = start.time,
-                            End.Time = end.time,
-                            Sections.to.Run = c( input$sectionsToRun1, input$sectionsToRun2, input$sectionsToRun3 ),
-                            Fig.Path = input$figureLocation,
-                            Ignore.Zones = c( input$ignoreZone1, input$ignoreZone2, input$ignoreZone3, input$ignoreZone4, input$ignoreZone5 ),
-                            Ignore.Regions = c( input$ignoreRegion1, input$ignoreRegion2, input$ignoreRegion3, input$ignoreRegion4, input$ignoreRegion5 ),
-                            Interfaces.for.Flows = c( input$interface1, input$interface2, input$interface3, input$interface4, input$interface5,
-                                                      input$interface6, input$interface7, input$interface8, input$interface9, input$interface10 ) 
+    write_data = list(  Database.Location = input$db.loc,
+                        DayAhead.Database.Location = input$DA.RT.db.loc,
+                        Using.Gen.Type.Mapping.CSV = genTypeMapping,
+                        reassign.zones = input$reassignZones,
+                        Gen.Region.Zone.Mapping.Filename = input$regionZoneMappingFile,
+                        CSV.Gen.Type.File.Location = input$genTypeMappingLocation,
+                        PLEXOS.Gen.Category = c( input$plexosCategory1,  input$plexosCategory2,  input$plexosCategory3,
+                                                 input$plexosCategory4,  input$plexosCategory5,  input$plexosCategory6,
+                                                 input$plexosCategory7,  input$plexosCategory8,  input$plexosCategory9,
+                                                 input$plexosCategory10, input$plexosCategory11, input$plexosCategory12,
+                                                 input$plexosCategory13, input$plexosCategory14, input$plexosCategory15 ),
+                        PLEXOS.Desired.Type = c( input$genType1,  input$genType2,  input$genType3,  input$genType4,  input$genType5,
+                                                 input$genType6,  input$genType7,  input$genType8,  input$genType9,  input$genType10,
+                                                 input$genType11, input$genType12, input$genType13, input$genType14, input$genType15 ),
+                        Gen.Type = c( input$genOrder1,  input$genOrder2,  input$genOrder3,  input$genOrder4,  input$genOrder5,  input$genOrder6,  input$genOrder7,
+                                      input$genOrder8,  input$genOrder9,  input$genOrder10, input$genOrder11, input$genOrder12, input$genOrder13, input$genOrder14,
+                                      input$genOrder15, input$genOrder16, input$genOrder17, input$genOrder18, input$genOrder19, input$genOrder20 ),
+                        Plot.Color = c( input$genTypeColor1,  input$genTypeColor2,  input$genTypeColor3,  input$genTypeColor4,  input$genTypeColor5, 
+                                        input$genTypeColor6,  input$genTypeColor7,  input$genTypeColor8,  input$genTypeColor9,  input$genTypeColor10, 
+                                        input$genTypeColor11, input$genTypeColor12, input$genTypeColor13, input$genTypeColor14, input$genTypeColor15, 
+                                        input$genTypeColor16, input$genTypeColor17, input$genTypeColor18, input$genTypeColor19, input$genTypeColor20 ),
+                        Gen.Order = c( input$genOrder1,  input$genOrder2,  input$genOrder3,  input$genOrder4,  input$genOrder5,  input$genOrder6,  input$genOrder7,
+                                       input$genOrder8,  input$genOrder9,  input$genOrder10, input$genOrder11, input$genOrder12, input$genOrder13, input$genOrder14,
+                                       input$genOrder15, input$genOrder16, input$genOrder17, input$genOrder18, input$genOrder19, input$genOrder20 ),
+                        Renewable.Types.for.Curtailment = c( input$reType1,  input$reType2,  input$reType3,  input$reType4,  input$reType5, 
+                                                             input$reType6,  input$reType7,  input$reType8,  input$reType9,  input$reType10,
+                                                             input$reType11, input$reType12, input$reType13, input$reType14, input$reType15, 
+                                                             input$reType16, input$reType17, input$reType18, input$reType19, input$reType20 ),	
+                        DA.RT.Plot.Types = c( input$DA_RT_Type1,  input$DA_RT_Type2,  input$DA_RT_Type3,  input$DA_RT_Type4,  input$DA_RT_Type5, 
+                                              input$DA_RT_Type6,  input$DA_RT_Type7,  input$DA_RT_Type8,  input$DA_RT_Type9,  input$DA_RT_Type10, 
+                                              input$DA_RT_Type11, input$DA_RT_Type12, input$DA_RT_Type13, input$DA_RT_Type14, input$DA_RT_Type15, 
+                                              input$DA_RT_Type16, input$DA_RT_Type17, input$DA_RT_Type18, input$DA_RT_Type19, input$DA_RT_Type20 ),	
+                        Key.Periods = c( input$keyPeriodName1, input$keyPeriodName2, input$keyPeriodName3, input$keyPeriodName4, 
+                                         input$keyPeriodName5, input$keyPeriodName6, input$keyPeriodName7, input$keyPeriodName8 ),
+                        Start.Time = start.time,
+                        End.Time = end.time,
+                        Sections.to.Run = c( input$sectionsToRun1, input$sectionsToRun2, input$sectionsToRun3 ),
+                        Fig.Path = input$figureLocation,
+                        Ignore.Zones = c( input$ignoreZone1, input$ignoreZone2, input$ignoreZone3, input$ignoreZone4, input$ignoreZone5 ),
+                        Ignore.Regions = c( input$ignoreRegion1, input$ignoreRegion2, input$ignoreRegion3, input$ignoreRegion4, input$ignoreRegion5 ),
+                        Interfaces.for.Flows = c( input$interface1, input$interface2, input$interface3, input$interface4, input$interface5,
+                                                  input$interface6, input$interface7, input$interface8, input$interface9, input$interface10 ) 
                         )
     
-    write_data_df = as.data.frame(stri_list2matrix(write_data))
+    write_data_df = as.data.frame(stri_list2matrix(write_data), stringsAsFactors=FALSE)
     names(write_data_df) = names(write_data)
     
-    write_data_df$Plot.Color[which(write_data_df$Gen.Type=="")]=NULL
+    write_data_df$Plot.Color[which(write_data_df$Gen.Type=="")]=NA
+    write_data_df$Start.Time[which(write_data_df$Key.Periods=="")]=NA
+    write_data_df$End.Time[which(write_data_df$Key.Periods=="")]=NA
 
+    write_data_df$Renewable.Types.for.Curtailment[which(write_data_df$Renewable.Types.for.Curtailment=="TRUE")] =
+      write_data_df$Gen.Order[which(write_data_df$Renewable.Types.for.Curtailment=="TRUE")]
+    write_data_df$Renewable.Types.for.Curtailment[which(write_data_df$Renewable.Types.for.Curtailment=="FALSE")]=NA
+    
+    write_data_df$DA.RT.Plot.Types[which(write_data_df$DA.RT.Plot.Types=="TRUE")] =
+      write_data_df$Gen.Order[which(write_data_df$DA.RT.Plot.Types=="TRUE")]
+    write_data_df$DA.RT.Plot.Types[which(write_data_df$DA.RT.Plot.Types=="FALSE")]=NA
+    
     write.csv(write_data_df, paste0(input$csvLocation,'/input_data.csv'), row.names=FALSE)
 
   })
