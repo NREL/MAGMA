4621b721137f6e09c6b65bb0656be54677370205
bstoll <brady.stoll@nrel.gov>
2017-02-02 10:37:02 -0700

    Updated example run files

--- Examples/run_html_output.R
+++ Examples/run_html_output.R
@@ -10,15 +10,15 @@ db.day.ahead.loc = '<Directory where the day ahead solution can be found.
 output.dir       = '<Directory where HTML reports should be saved>'
 fig.path.name    = '<Path to save figures>'
 output.name      = '<Desired output file name. Include .html at end.>'
-query.data       = '<TRUE if you want to create new queries or 
+query.data       = TRUE 'if you want to create new queries or 
                      FALSE if you want to load existing data>'
 load.data        = '<Name of file to load if query.data=FALSE >'
-save.data        = '<TRUE if you want to save all data at the end of MAGMA Run
+save.data        = TRUE 'if you want to save all data at the end of MAGMA Run
                      FALSE if you do not want to save data. Only html will be produced>'
 save.data.name   = '<Name of file to save data. Will save in output.dir>'
-reassign.zones   = '<TRUE if reassigning zones from zones in PLEXOS
+reassign.zones   = TRUE 'if reassigning zones from zones in PLEXOS
                      FALSE if you want to use PLEXOS zones.>'
-use.gen.type.csv = '<TRUE if assigning generator type with a CSV file.
+use.gen.type.csv = TRUE 'if assigning generator type with a CSV file.
                      FALSE if assigning generator type based on PLEXOS categories.>'
 gen.type.csv.loc = '<Location of CSV mapping generator name to type.
                      NULL if above is false.>'
